a7d4e51f2d54fe41775b4d9085f382c6c1525b6e
Muguku <hello@muguku.co.ke>
2020-12-12 02:15:58 +0300

    add dynamic single article view

--- pages/top-headlines.js
+++ pages/top-headlines.js
@@ -1,5 +1,6 @@
 import { QueryCache, useQuery } from "react-query";
 import { dehydrate } from "react-query/hydration";
+import Link from "next/link";
 import HomeTemplate from "../components/homeTemplate";
 import MediaCard from "../components/mediaCard";
 import { getTopHeadlines } from "../api/fetchNews";
@@ -46,18 +47,41 @@ export default function topHeadlines() {
         {/* return columns of articles */}
         {data.articles.map(
           (
-            { urlToImage, title, description, author, publishedAt, content },
+            {
+              urlToImage,
+              title,
+              description,
+              author,
+              publishedAt,
+              url,
+              source: { name },
+            },
             index
           ) => (
-            <MediaCard
+            <div
+              className="uko_wapi my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3"
               key={index}
-              image={urlToImage}
-              title={title}
-              description={description}
-              author={author}
-              date={publishedAt}
-              content={content}
-            />
+            >
+              {/* compose dynamic url made up of hyphen separated publication name and index in response array */}
+              {/* eg /post/the-washington-post?pid=0 */}
+              <Link
+                href={{
+                  pathname: `post/${name.toLowerCase().split(" ").join("-")}`,
+                  query: { pid: index },
+                }}
+              >
+                <a className="cursor-pointer">
+                  <MediaCard
+                    image={urlToImage}
+                    title={title}
+                    description={description}
+                    author={author}
+                    date={publishedAt}
+                    url={url}
+                  />
+                </a>
+              </Link>
+            </div>
           )
         )}
       </div>
