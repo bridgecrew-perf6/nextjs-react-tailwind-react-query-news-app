c97a6fd7dbfb84e4a7f8ac3cda8f0610b84041ab
Muguku <hello@muguku.co.ke>
2020-12-11 22:03:37 +0300

    add cache and hydration mechanism for react query SSR

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/index.css'
+import { ReactQueryCacheProvider, QueryCache } from "react-query";
+import { Hydrate } from "react-query/hydration";
+import "../styles/index.css";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const queryCache = new QueryCache();
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <ReactQueryCacheProvider queryCache={queryCache}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+      </Hydrate>
+    </ReactQueryCacheProvider>
+  );
 }
-
-export default MyApp
