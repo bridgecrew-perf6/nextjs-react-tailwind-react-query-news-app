d8f91eb9487fbe199303e3c278e6fe3483b4dd51
Muguku <hello@muguku.co.ke>
2020-12-11 14:29:08 +0300

    style children container

--- components/homeTemplate.js
+++ components/homeTemplate.js
@@ -15,10 +15,8 @@ export default function homeTemplate({ children, title, activeLink }) {
       <main>
         <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
           {/* <!-- Content goes here --> */}
-          <div className="px-4 py-6 sm:px-0">
-            <div className="border-2 border-dashed border-gray-200 rounded-lg h-96">
-              {children}
-            </div>
+          <div className="container my-12 mx-auto px-4 md:px-12">
+            {children}
           </div>
           {/* <!-- /End Content --> */}
         </div>
