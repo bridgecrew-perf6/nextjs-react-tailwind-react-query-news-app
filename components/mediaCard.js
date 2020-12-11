b45ed43d6cfdbcb0df16e93dc7951ad3faa383f3
Muguku <hello@muguku.co.ke>
2020-12-11 22:04:35 +0300

    replace generic media card info with props and placeholder image

--- components/mediaCard.js
+++ components/mediaCard.js
@@ -1,51 +1,54 @@
-export default function mediaCard() {
+import Link from "next/link";
+import BrokenImage from "../assets/broken_image.png";
+
+export default function mediaCard({
+  image,
+  title,
+  description,
+  author,
+  date,
+  content,
+}) {
   return (
-    <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
-      <article className="overflow-hidden rounded-lg shadow-lg">
-        <a href="#">
+    <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3 cursor-pointer">
+      <Link href="/">
+        <article className="overflow-hidden rounded-lg shadow-lg">
           {/* Article image */}
           <img
-            alt="Placeholder"
-            className="block h-auto w-full"
-            src="https://www.washingtonpost.com/wp-apps/imrs.php?src=https://arc-anglerfish-washpost-prod-washpost.s3.amazonaws.com/public/5CVPI3RBOMI6XLKTJQP5USMQPU.jpg&w=1440"
+            alt="article image"
+            className="block h-48 w-full"
+            src={image ? image : BrokenImage}
           />
-        </a>
-        <header className="flex items-center leading-tight p-2">
-          <h1 className="text-lg font-bold">
-            {/* Article title */}
-            <a className="no-underline hover:underline text-black" href="#">
-              European stocks stumble on COVID-19 vaccine setback, while U.S.
-              equity futures fall on stimulus worries - MarketWatch
-            </a>
-          </h1>
-        </header>
-        <section className="px-4">
-          {/* Article description */}
-          <p className="text-base">
-            Patriots coach Bill Belichick said Cam Newton will remain the
-            starting quarterback despite another shaky performance in Thursday
-            night's 24-3 loss to the Rams.
-          </p>
-        </section>
-        <footer className="flex items-center justify-between leading-none p-2 md:p-4">
-          <a
-            className="flex items-center no-underline hover:underline text-black"
-            href="#"
-          >
-            {/* Article author */}
-            <p className="ml-2 text-sm">Sareen Habeshian</p>
-          </a>
-          <a
-            className="no-underline text-grey-darker hover:text-red-dark"
-            href="#"
-          >
-            {/* Article date */}
-            <p className="text-grey-darker text-sm">
-              {new Date("2020-12-11T03:02:00Z").toDateString()}
+          <header className="flex items-center leading-tight p-2 h-32 lg:h-36">
+            <h1 className="text-lg font-bold">
+              {/* Article title */}
+              <span className=" text-black">
+                {title ? title : "No Title Found"}
+              </span>
+            </h1>
+          </header>
+          <section className="px-4 h-28 lg:h-32">
+            {/* Article description */}
+            <p className="text-base">
+              {description ? description : "No Description Found"}
             </p>
-          </a>
-        </footer>
-      </article>
+          </section>
+          <footer className="flex items-center justify-between leading-none p-2 md:p-4 h-14">
+            <h4 className="flex items-center  text-black">
+              {/* Article author */}
+              <p className="ml-2 text-sm">
+                {author ? author : "No Author Name"}
+              </p>
+            </h4>
+            <h4 className=" text-grey-darker hover:text-red-dark">
+              {/* Article date */}
+              <p className="text-grey-darker text-sm">
+                {date ? new Date(date).toDateString() : "No Date Given"}
+              </p>
+            </h4>
+          </footer>
+        </article>
+      </Link>
     </div>
   );
 }
