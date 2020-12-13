d6ff70bbd4e4e4c1be9968456db6d7bac05b1ed3
Muguku <hello@muguku.co.ke>
2020-12-14 02:01:48 +0300

    implement custom news feed based on user preference

--- pages/custom-news-feed.js
+++ pages/custom-news-feed.js
@@ -1,11 +1,26 @@
-import React from "react";
-import HomeTemplate from "../components/homeTemplate";
+import { useQuery } from "react-query";
+import FeedTemplate from "../components/feedTemplate";
+import { getCustomNewsFeed } from "../api/fetchNews";
 
-export default function newsFeed() {
+export default function customNewsFeed() {
+  // get preferred topic from local storage
+  // check for window object incase of SSR
+  let PREFERRED_TOPIC =
+    typeof window !== "undefined" ? localStorage.getItem("newsPreference") : "";
+
+  // fetch Custom News Feed
+  const { isLoading, isError, data } = useQuery(
+    "custom_news_feed",
+    getCustomNewsFeed(PREFERRED_TOPIC)
+  );
+
   return (
-    <HomeTemplate title="Custom News Feed" activeLink="Custom News Feed">
-      something else
-      <div>another something else</div>
-    </HomeTemplate>
+    <FeedTemplate
+      isLoading={isLoading}
+      isError={isError}
+      data={data}
+      homeTemplateTitle="Custom News Feed"
+      category="feed"
+    />
   );
 }
